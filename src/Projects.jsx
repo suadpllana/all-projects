7bbc37b52668f5392f7f45d329786af244ff1727
oltapllana <oltapllana@gmail.com>
2025-01-22 04:16:47 +0100

    first commit

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,311 +1,59 @@
-import React from 'react'
-import {useState} from "react"
+import React from "react";
+import { useState } from "react";
+import { data } from "./data";
+import ai from "./images/ai.png";
 const Projects = () => {
-     const [toggleView , setToggleView] = useState(true)
+  const [toggleView, setToggleView] = useState(true);
+  const [storedData, setStoredData] = useState(data)
+  const [complexity, setComplexity] = useState()
 
+  
 
+     function filterProjects(complexity){
+          if(complexity === ""){
+               setStoredData(data);
+               return
+          }
+          const filteredProjects = data.filter(project => project.type === complexity);
+          setStoredData(filteredProjects)
+     }
 
   return (
     <div className={toggleView ? "all-projects-container " : " darkBody"}>
-      <h1 className={!toggleView && "lightH1"}>Hello my name is Suad Pllana , passionate Front-end Developer</h1>
-      <h2 className={!toggleView && "lightH1"}>Here are all my projects </h2>
-      <button className={toggleView ? "blackButton" : ""} onClick={() => setToggleView((prev) => !prev)}>{toggleView ? "Dark Mode" : "Light Mode"}</button>
-        <div className="container" id={!toggleView ? "darkContainer" : ""}>
+      <h1 className={!toggleView && "lightH1"}>
+        My name is Suad Pllana , a passionate front-end developer
+      </h1>
+      <h2 className={!toggleView && "lightH1"}>Check all of my projects below </h2>
+      <h3>Filter by <select onChange={(e) => filterProjects(e.target.value)}>
+               <option value="">Get all projects</option>
+               <option value="">Alphabetic order</option>
+               <option value="complex">Complex Projects</option>
+               <option value="intermediate">Intermediate Projects</option>
+               <option value="easy">Easy Projects</option>
+          </select></h3>
 
-           <a target="_blank" href="https://suadpllana.github.io/ai-image-generator/">
-            <div>
-                 <h2>AI Image Generator </h2>
+      <div className="container" id={!toggleView ? "darkContainer" : ""}>
+        {storedData &&
+          storedData.map((project, index) => (
+            <div className="project">
+              <img src={project.image} alt="" />
+              <div className="content">
+                <h3>{project.title}</h3>
+                <p>{project.tools}</p>
+                <a 
+                  target="_blank"
+                  href={project.link}
+                >
+                  Link to project
+                </a>
+              </div>
             </div>
-            </a>
+          ))}
 
-            <a target="_blank" href="https://suadpllana.github.io/books">
-            <div>
-                 <h2>Books</h2>
-            </div>
-            </a>
-
-
-            <a target="_blank" href="https://suadpllana.github.io/bmi">
-            <div>
-                 <h2>BMI Calculator</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/calculator">
-            <div>
-                 <h2>Calculator</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/calendar/">
-            <div>
-                 <h2>Calendar</h2>
-            </div>
-            </a>
-
-
-            <a target="_blank" href="https://suadpllana.github.io/charity/">
-            <div>
-                 <h2>Charity</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/cinema/">
-            <div>
-                 <h2>Cinema</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/coffee/">
-            <div>
-                 <h2>Coffee</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/color-picker/">
-            <div>
-                 <h2>Color Picker</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/crypto-prices/">
-            <div>
-                 <h2>Crypto Prices</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/currency-converter/">
-            <div>
-                 <h2>Currency Converter </h2>
-            </div>
-            </a>
-
-           
-
-            <a target="_blank" href="https://suadpllana.github.io/dictionary/">
-            <div>
-                 <h2>Dictionary</h2>
-            </div>
-            </a>
-            <a target="_blank" href="https://suadpllana.github.io/digital-clock/">
-            <div>
-                 <h2>Digital Clock</h2>
-            </div>
-            </a>
-
-
-            <a target="_blank" href="https://suadpllana.github.io/dogs/">
-            <div>
-                 <h2>Dogs</h2>
-            </div>
-            </a>
-
-
-            <a target="_blank" href="https://suadpllana.github.io/drums/">
-            <div>
-                 <h2>Drums </h2>
-            </div>
-            </a>
-
-
-          
-
-            <a target="_blank" href="https://suadpllana.github.io/euro2024/">
-            <div>
-                 <h2>Euro 2024</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/expense-tracker/">
-            <div>
-                 <h2>Expense Tracker</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/free-games/">
-            <div>
-                 <h2>Free Games</h2>
-            </div>
-            </a>
-
-
-            <a target="_blank" href="https://suadpllana.github.io/header/">
-            <div>
-                 <h2>Header</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/holidays/">
-            <div>
-                 <h2>Holidays</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/image-gallery/">
-            <div>
-                 <h2>Image Gallery</h2>
-            </div>
-            </a>
-
-            
-            <a target="_blank" href="https://suadpllana.github.io/jokes/">
-            <div>
-                 <h2>Jokes</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/logo-generator/">
-            <div>
-                 <h2>Logo</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/music/">
-            <div>
-                 <h2>Music</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/naruto/">
-            <div>
-                 <h2>Naruto </h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/nasa/">
-            <div>
-                 <h2>Nasa</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/news/">
-            <div>
-                 <h2>News</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/notes/">
-            <div>
-                 <h2>Notes</h2>
-            </div>
-            </a>
-            <a target="_blank" href="https://suadpllana.github.io/number-converter/">
-            <div>
-                 <h2>Number Converter</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/piano/">
-            <div>
-                 <h2>Piano </h2>
-            </div>
-            </a>
-
-            
-            <a target="_blank" href="https://suadpllana.github.io/pokemon/">
-            <div>
-                 <h2>Pokemon</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/quiz/">
-            <div>
-                 <h2>Quiz</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/qr-code-generator/">
-            <div>
-                 <h2>QR code Generator</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/quotes/">
-            <div>
-                 <h2>Quotes </h2>
-            </div>
-            </a>
-
-           
-
-
-
-            <a target="_blank" href="https://suadpllana.github.io/quran/">
-            <div>
-                 <h2>Quran</h2>
-            </div>
-            </a>
-
-
-            <a target="_blank" href="https://suadpllana.github.io/random-password-generator/">
-            <div>
-                 <h2>Random Password Generator</h2>
-            </div>
-            </a>
-
-            
-            <a target="_blank" href="https://suadpllana.github.io/restaurant/">
-            <div>
-                 <h2>Restaurant</h2>
-            </div>
-            </a>
-          
-          
-            <a target="_blank" href="https://suadpllana.github.io/rockPaperScissors/">
-            <div>
-                 <h2>Rock Paper Scissors</h2>
-            </div>
-            </a>
-                    
-           
-
-            <a target="_blank" href="https://suadpllana.github.io/stopwatch/">
-            <div>
-                 <h2>Stopwatch</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/text-to-voice-converter/">
-            <div>
-                 <h2>Text to Voice Converter </h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/to-do-list/">
-            <div>
-                 <h2>To do list</h2>
-            </div>
-            </a>
-
-            <a target="_blank" href="https://suadpllana.github.io/translate/">
-            <div>
-                 <h2>Translation </h2>
-            </div>
-            </a>
-
-            
-            
-
-         
-
-            <a target="_blank" href="https://suadpllana.github.io/university/">
-            <div>
-                 <h2>University</h2>
-            </div>
-            </a>
-             
-        <a target="_blank" href="https://suadpllana.github.io/weather-app/">
-            <div>
-                 <h2>Weather </h2>
-            </div>
-            </a>
-
-           
-         
-          
-        </div>
+    
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
---
+++ src/data.js
@@ -0,0 +1,410 @@
+import ai from "./images/ai.png"
+import bmi from "./images/bmi.png"
+import books from "./images/books.png"
+import calculator from "./images/calculator.png"
+import calendar from "./images/calendar.png"
+import charity from "./images/charity.png"
+import cinema from "./images/cinema.png"
+import coffee from "./images/coffee.png"
+import colorPicker from "./images/colorPicker.png"
+import cryptoPrices from "./images/cryptoPrices.png"
+import currency from "./images/currency.png"
+import dictionary from "./images/dictionary.png"
+import clock from "./images/clock.png"
+import dogs from "./images/dogs.png"
+import drums from "./images/drums.png"
+import euro from "./images/euro2024.png"
+import expenseTracker from "./images/expenseTracker.png"
+import games from "./images/games.png"
+import header from "./images/header.png"
+import holidays from "./images/holidays.png"
+import imageGallery from "./images/imageGallery.png"
+import jokes from "./images/jokes.png"
+import logo from "./images/logo.png"
+import music from "./images/music.png"
+import naruto from "./images/naruto.png"
+import nasa from "./images/nasa.png"
+import news from "./images/news.png"
+import notes from "./images/notes.png"
+import numberConverter from "./images/numberConverter.png"
+import movies from "./images/movies.png"
+import piano from "./images/piano.png"
+import pokemon from "./images/pokemon.png"
+import quiz from "./images/quiz.png"
+import qrCode from "./images/qrCode.png"
+import quotes from "./images/quotes.png"
+import quran from "./images/quran.png"
+import password from "./images/password.png"
+import restaurant from "./images/restaurant.png"
+import rockPaperScissors from "./images/rockPaperScissors.png"
+import stopwatch from "./images/stopwatch.png"
+import textToSpeech from "./images/textToSpeech.png"
+import todo from "./images/toDoList.png"
+import translation from "./images/translation.png"
+import university from "./images/university.png"
+import weather from "./images/weather.png"
+
+export const data = [
+    {
+        id:0,
+        image: ai,
+        title: "AI Image Generator",
+        tools: "React, Sass",
+        link: "https://suadpllana.github.io/ai-image-generator",
+        type: "complex"
+    },
+    {
+        id:1,
+        image: books,
+        title: "Books",
+        tools: "React",
+        link: "https://suadpllana.github.io/books",
+        type: "complex"
+    },
+    {
+        id:2,
+        image: bmi,
+        title: "BMI Calculator",
+        tools: "React",
+        link: "https://suadpllana.github.io/bmi",
+        type: "easy"
+    },
+    {
+        id:3,
+        image: calculator,
+        title: "Calculator",
+        tools: "React",
+        link: "https://suadpllana.github.io/calculator",
+        type: "easy"
+    },
+    {
+        id:4,
+        image: calendar,
+        title: "Calendar",
+        tools: "React",
+        link: "https://suadpllana.github.io/calendar",
+        type: "easy",
+    },
+    {
+        id:5,
+        image: charity,
+        title: "Charity",
+        tools: "React, Bootstrap",
+        link: "https://suadpllana.github.io/charity",
+        type: "intermediate"
+    },
+    {
+        id:6,
+        image: cinema,
+        title: "Cinema",
+        tools: "React, Sass",
+        link: "https://suadpllana.github.io/cinema",
+        type: "intermediate"
+    },
+    {
+        id:7,
+        image: coffee,
+        title: "Coffee",
+        tools: "React",
+        link: "https://suadpllana.github.io/coffee",
+        type: "easy"
+    },
+    {
+        id:8,
+        image: colorPicker,
+        title: "Color Picker",
+        tools: "React",
+        link: "https://suadpllana.github.io/color-picker",
+        type: "intermediate"
+    },
+    {
+        id:9,
+        image: cryptoPrices,
+        title: "Crypto Prices",
+        tools: "React",
+        link: "https://suadpllana.github.io/crypto-prices",
+        type: "complex"
+    },
+    {
+        id:10,
+        image: currency,
+        title: "Currency Converter",
+        tools: "React, Tailwind CSS",
+        link: "https://suadpllana.github.io/currency-converter",
+        type: "intermediate"
+    },
+    {
+        id:11,
+        image: dictionary,
+        title: "Dictionary",
+        tools: "React, Bootstrap",
+        link: "https://suadpllana.github.io/dictionary",
+        type: "intermediate"
+    },
+    {
+        id:12,
+        image: clock,
+        title: "Digital Clock",
+        tools: "React",
+        link: "https://suadpllana.github.io/digital-clock",
+        type: "easy"
+    },
+    {
+        id:13,
+        image: dogs,
+        title: "Dogs",
+        tools: "React",
+        link: "https://suadpllana.github.io/dogs",
+        type: "intermediate"
+    },
+    {
+        id:14,
+        image: drums,
+        title: "Drums",
+        tools: "React",
+        link: "https://suadpllana.github.io/drums",
+        type: "intermediate"
+    },
+    {
+        id:15,
+        image: euro,
+        title: "Euro 2024",
+        tools: "React",
+        link: "https://suadpllana.github.io/euro2024",
+        type: "complex"
+    },
+    {
+        id:16,
+        image: expenseTracker,
+        title: "Expense tracker",
+        tools: "React",
+        link: "https://suadpllana.github.io/expense-tracker",
+        type: "intermediate"
+    },
+    {
+        id:17,
+        image: games,
+        title: "Games",
+        tools: "React",
+        link: "https://suadpllana.github.io/games",
+        type: "complex"
+    },
+    {
+        id:18,
+        image: header,
+        title: "Header",
+        tools: "React",
+        link: "https://suadpllana.github.io/header",
+        type: "easy"
+    },
+    {
+        id:19,
+        image: holidays,
+        title: "Holidays",
+        tools: "React",
+        link: "https://suadpllana.github.io/holidays",
+        type: "easy"
+    },
+    {
+        id:20,
+        image: imageGallery,
+        title: "Image Gallery",
+        tools: "React",
+        link: "https://suadpllana.github.io/image-gallery",
+        type: "intermediate"
+    },
+    {
+        id:21,
+        image: jokes,
+        title: "Jokes",
+        tools: "React",
+        link: "https://suadpllana.github.io/jokes",
+        type: "intermediate"
+    },
+    {
+        id:22,
+        image: logo,
+        title: "Logo Generator",
+        tools: "React",
+        link: "https://suadpllana.github.io/logo-generator",
+        type: "easy"
+    },
+    {
+        id: 23,
+        image: movies,
+        title: "Movies",
+        tools: "React",
+        link: "https://suadpllana.github.io/movies",
+        type: "complex"
+    },
+    {
+        id:24,
+        image: music,
+        title: "Music",
+        tools: "React",
+        link: "https://suadpllana.github.io/music",
+        type: "complex"
+    },
+    {
+        id:25,
+        image: naruto,
+        title: "Naruto",
+        tools: "React",
+        link: "https://suadpllana.github.io/naruto",
+        type: "complex"
+    },
+    {
+        id:26,
+        image: nasa,
+        title: "Nasa",
+        tools: "React",
+        link: "https://suadpllana.github.io/holidays",
+        type: "intermediate"
+    },
+    {
+        id:27,
+        image: news,
+        title: "News",
+        tools: "React",
+        link: "https://suadpllana.github.io/news",
+        type: "complex"
+    },
+    {
+        id:28,
+        image: notes,
+        title: "Notes",
+        tools: "React",
+        link: "https://suadpllana.github.io/notes",
+        type: "intermediate"
+    },
+    {
+        id:29,
+        image: numberConverter,
+        title: "Number Converter",
+        tools: "React",
+        link: "https://suadpllana.github.io/number-converter",
+        type: "intermediate"
+    },
+    {
+        id:30,
+        image: piano,
+        title: "Piano",
+        tools: "React",
+        link: "https://suadpllana.github.io/piano",
+        type: "intermediate"
+    },
+    {
+        id:31,
+        image: pokemon,
+        title: "Pokemon",
+        tools: "React",
+        link: "https://suadpllana.github.io/pokemon",
+        type: "complex"
+    },
+    {
+        id:32,
+        image: quiz,
+        title: "Quiz",
+        tools: "React",
+        link: "https://suadpllana.github.io/quiz",
+        type: "intermediate"
+    },
+    {
+        id:33,
+        image: qrCode,
+        title: "Qr Code Generator",
+        tools: "React",
+        link: "https://suadpllana.github.io/qr-code-generator",
+        type: "easy"
+    },
+    {
+        id:34,
+        image: quotes,
+        title: "Quotes",
+        tools: "React",
+        link: "https://suadpllana.github.io/quotes",
+        type: "complex"
+    },
+    {
+        id:35,
+        image: quran,
+        title: "Quran",
+        tools: "React",
+        link: "https://suadpllana.github.io/quran",
+        type: "complex"
+    },
+    {
+        id:36,
+        image: password,
+        title: "Random Password Generator",
+        tools: "React",
+        link: "https://suadpllana.github.io/random-password-generator",
+        type: "easy"
+    },
+    {
+        id:37,
+        image: restaurant,
+        title: "Restaurant",
+        tools: "React",
+        link: "https://suadpllana.github.io/restaurant",
+        type: "complex"
+    },
+    {
+        id:38,
+        image: rockPaperScissors,
+        title: "Rock Paper Scissors",
+        tools: "HTML,CSS,Javascript",
+        link: "https://suadpllana.github.io/rockPaperScissors",
+        type: "easy"
+    },
+    {
+        id:39,
+        image: stopwatch,
+        title: "Stopwatch",
+        tools: "React",
+        link: "https://suadpllana.github.io/stopwatch",
+        type: "easy"
+    },
+    {
+        id:40,
+        image: textToSpeech,
+        title: "Text to Speech Converter",
+        tools: "React",
+        link: "https://suadpllana.github.io/text-to-voice-converter",
+        type: "intermediate"
+    },
+    {
+        id:41,
+        image: todo,
+        title: "To do list",
+        tools: "React",
+        link: "https://suadpllana.github.io/to-do-list",
+        type: "complex"
+    },
+    {
+        id:42,
+        image: translation,
+        title: "Translation",
+        tools: "React, Tailwind CSS",
+        link: "https://suadpllana.github.io/translate",
+        type: "complex"
+    },
+    {
+        id:43,
+        image: university,
+        title: "University",
+        tools: "React",
+        link: "https://suadpllana.github.io/university",
+        type: "intermediate"
+    },
+    {
+        id:44,
+        image: weather,
+        title: "Weather",
+        tools: "React",
+        link: "https://suadpllana.github.io/weather-app",
+        type: "intermediate"
+    },
+  
+   
+]
